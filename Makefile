1da32fa560452ca91dc86c9b8def777f9e53d567
MrDumbrava <109512837+9xbt@users.noreply.github.com>
2023-06-24 10:10:53 +0200

    Some more changes

--- Makefile
+++ Makefile
@@ -1,13 +1,9 @@
 all: build
-	qemu-system-i386 -cdrom kernel.iso
+	/usr/bin/qemu/./qemu-system-i386 -cdrom kernel.iso
 
 build: clean
-	touch kernel.bin
 	nasm -f bin -o kernel.bin kernel.asm
-	dd if=kernel.bin of=kernel.img
-	mkdir -v cdiso
-	mv -v kernel.img cdiso
-	mkisofs -b kernel.img -no-emul-boot -o kernel.iso cdiso/
+	mkisofs -b kernel.bin -no-emul-boot -o kernel.iso .
 
 clean:
 	rm -rvf kernel.bin
--- kernel.asm
+++ kernel.asm
@@ -1,315 +1,314 @@
 [BITS 16]
 org 0x7C00
 
-jmp main
-
-; variables
-startup_logo db ` __  _______    ______    ______  \r\n|  \\|       \\  /      \\  /      \\ \r\n \\$$| $$$$$$$\\|  $$$$$$\\|  $$$$$$\\\r\n|  \\| $$  | $$| $$  | $$| $$___\\$$\r\n| $$| $$  | $$| $$  | $$ \\$$    \\ \r\n| $$| $$  | $$| $$  | $$ _\\$$$$$$\\\r\n| $$| $$__/ $$| $$__/ $$|  \\__| $$\r\n| $$| $$    $$ \\$$    $$ \\$$    $$\r\n \\$$ \\$$$$$$$   \\$$$$$$   \\$$$$$$ \r\n\n\0`
-msg_boot_successful db `Welcome to imperiumDOS!\r\n\0`
-msg_version db `Beta 1.2-pre\r\nCopyright (c) 2023 Imperium. All rights reserved\r\n\n\0`
-
-buffer times 0x4D db 0
-cmd_memo_buffer times 0x48 db 0
-prompt db `$ \0`
-nl db `\r\n\0`
-
-cmd_credits db `credits\0`
-cmd_help db `help\0`
-cmd_about db `about\0`
-cmd_clear db `clear\0`
-cmd_echo db `echo`
-cmd_memo db `memo`
-
-err_unknown db `Unknown command.\r\n\0`
-err_argument db `Not enough arguments.\r\n\0`
-
-msg_about_1 db `-- iDOS --\r\n\0`
-msg_about_2 db ` Beta 1.2-pre\r\n Copyright (c) 2023 Imperium\r\n\n\0`
-msg_help_1 db `-- Functions --\r\n\0`
-msg_help_2 db ` help - Shows all functions.\r\n about - Shows information about the project.\r\n clear - Clears the screen.\r\n echo - Echoes what you say.\r\n credits - Shows the credits.\r\n edit - Text editor.\r\n\n\0`
-msg_credits_1 db `-- Credits --\r\n\0`
-msg_credits_2 db ` xrc2 - Created the project.\r\n ekeleze - Contributor.\r\n\n\0`
+jmp _start 
 
 %macro write 1
 mov si, %1
-mov bl, 0x07
+mov bl, $07
 call print_string
 %endmacro
 
 %macro cwrite 2
-mov si, %1
-call len_string
-
 mov bl, %2
-mov cx, si
-call set_color
-
-write %1
+mov si, %1
+call print_string
 %endmacro
 
 %macro log 1
-write %1
-write `\r\n`
+mov bl, 0x07
+mov si, %1
+call print_string
 %endmacro
 
 %macro clog 2
-cwrite %1, %2
-write nl
+mov bl, %2
+mov si, %1
+call print_string
+mov bl, $07
+mov si, nl
+call print_string
 %endmacro
 
-SECTION .text:
-  global main
+section .data:
+  startup_logo: db ` __  _______    ______    ______  \r\n|  \\|       \\  /      \\  /      \\ \r\n \\$$| $$$$$$$\\|  $$$$$$\\|  $$$$$$\\\r\n|  \\| $$  | $$| $$  | $$| $$___\\$$\r\n| $$| $$  | $$| $$  | $$ \\$$    \\ \r\n| $$| $$  | $$| $$  | $$ _\\$$$$$$\\\r\n| $$| $$__/ $$| $$__/ $$|  \\__| $$\r\n| $$| $$    $$ \\$$    $$ \\$$    $$\r\n \\$$ \\$$$$$$$   \\$$$$$$   \\$$$$$$ \r\n\n\0`
+  msg_boot_successful: db `Welcome to imperiumDOS!\r\n\0`
+  msg_version: db `Beta 1.2-pre\r\nCopyright (c) 2023 Imperium. All rights reserved\r\n\n\0`
 
-main:
-  ; clear the registers
-  mov ax, 0
-  mov ds, ax
-  mov es, ax
-  mov ss, ax
-  ; set the stack at 0x7C00
-  mov sp, 0x7C00
+  prompt: db `$ \0`
+  nl: db `\r\n\0`
 
-  ; clear the screen
-  mov ax, 3
-  int 10h
+  cmd_credits: db `credits\0`
+  cmd_help: db `help\0`
+  cmd_about: db `about\0`
+  cmd_clear: db `clear\0`
+  cmd_echo: db "echo"
+  cmd_user: db "user"
 
-  ; show startup screen
-  log msg_boot_successful
-  mov bl, 0x02
-  mov cx, 674
-  call set_color
-  log startup_logo
-  log msg_version
+  err_unknown: db `Unknown command.\r\n\0`
+  err_argument: db `Not enough arguments.\r\n\0`
 
-  jmp _loop ; go to main loop
+  msg_about_1: db `-- iDOS --\r\n\0`
+  msg_about_2: db ` Beta 1.2-pre\r\n Copyright (c) 2023 Imperium\r\n\n\0`
+  msg_help_1: db `-- Functions --\r\n\0`
+  msg_help_2: db ` help - Shows all functions.\r\n about - Shows information about the project.\r\n clear - Clears the screen.\r\n echo - Echoes what you say.\r\n credits - Shows the credits.\r\n user - Sets your username for this session.\r\n\n\0`
+  msg_credits_1: db `-- Credits --\r\n\0`
+  msg_credits_2: db ` xrc2 - Created the project.\r\n ekeleze - Contributor.\r\n\n\0`
 
-_loop:
-  mov si, prompt
-  call print_string
+  input_buffer: times 0x4D db 0
+  user_buffer: times 0x4D db 0
 
-  mov di, buffer
-  call get_string
-
-  mov si, buffer
-  cmp byte [si], 0
-  je _loop
-
-  mov si, buffer
-  mov di, cmd_help
-  mov cl, 0x00
-  call cmp_string
-  jc .help
- 
-  mov si, buffer
-  mov di, cmd_about
-  mov cl, 0x00
-  call cmp_string
-  jc .about
-
-  mov si, buffer
-  mov di, cmd_clear
-  mov cl, 0x00
-  call cmp_string
-  jc .clear
-
-  mov si, buffer
-  mov di, cmd_echo
-  mov cl, 'o'
-  call cmp_string
-  jc .echo
-
-  mov si, buffer
-  mov di, cmd_credits
-  mov cl, 0x00
-  call cmp_string
-  jc .credits
-
-  mov si, buffer
-  mov di, cmd_memo
-  mov cl, 'o'
-  call cmp_string
-  jc .memo
-
-  jc .err_unknown
- 
-  ; Commands section
-
-  .err_unknown:
-    cwrite err_unknown, $0C
-    jmp _loop
-
-  .err_argument:
-    cwrite err_argument, $0C
-    jmp _loop
-
-  .help:
-    write nl
-    cwrite msg_help_1, $02
-    write msg_help_2
-    jmp _loop
-
-  .about:
-    write nl
-    cwrite msg_about_1, $02
-    log msg_about_2
-    jmp _loop
-
-  .credits:
-    write nl
-    cwrite msg_credits_1, $02
-    log msg_credits_2
-    jmp _loop
-
-  .clear:
+section .text:
+  _start:
+    ; clear the registers
+    mov ax, 0
+    mov ds, ax
+    mov es, ax
+    mov ss, ax
+    ; set the stack at 0x7C00
+    mov sp, 0x7C00
+  
+    ; clear the screen
     mov ax, 3
     int 10h
-    jmp _loop
 
-  .echo:
-    log buffer + 5
-    write nl
-    jmp _loop
+    ; show startup screen
+    log msg_boot_successful
+    mov si, startup_logo
+    mov bl, 0x02
+    call print_string
+    log msg_version
 
-  .memo:
-    
-    jmp _loop
+    jmp _loop ; go to main loop
 
-set_color:
-  ; usage:
-  ; mov bl, your_color
-  ; mov cx, your_string_length
-  mov ah, 09h
-  int 10h
-  ret
+  _loop:
+    mov si, prompt
+    call print_string
 
-clear_screen:
-  mov ax, 3
-  int 10h
-  ret
+    mov di, input_buffer
+    call get_string
+  
+    mov si, input_buffer
+    cmp byte [si], 0
+    je _loop
 
-print_string:
-  ; si = string pointer
-  ; bl = color
-
-  lodsb
-  or al, al
-  jz .ret
-
-  ;mov cx, 1
-  ;call set_color
+    mov di, cmd_help
+    mov cl, 0x00
+    call cmp_string
+    jc .help
  
-  mov ah, $0e
-  int $10
+    mov di, cmd_about
+    mov cl, 0x00
+    call cmp_string
+    jc .about
 
-  jmp print_string
+    mov di, cmd_clear
+    mov cl, 0x00
+    call cmp_string
+    jc .clear
 
-  .ret:
+    mov di, cmd_echo
+    mov cl, 'o'
+    call cmp_string
+    jc .echo
+
+    mov di, cmd_credits
+    mov cl, 0x00
+    call cmp_string
+    jc .credits
+
+    mov di, cmd_user
+    mov cl, 'r'
+    call cmp_string
+    jc .user
+
+    jc .unknown
+ 
+    ; Commands section
+
+    .unknown:
+      cwrite err_unknown, $0C
+      jmp _loop
+
+    .help:
+      write nl
+      cwrite msg_help_1, $02
+      write msg_help_2
+      jmp _loop
+
+    .about:
+      write nl
+      cwrite msg_about_1, $02
+      log msg_about_2
+      jmp _loop
+
+    .credits:
+      write nl
+      cwrite msg_credits_1, $02
+      log msg_credits_2
+      jmp _loop
+
+    .clear:
+      mov ax, 3
+      int 10h
+      jmp _loop
+
+    .echo:
+      log input_buffer + 5
+      write nl
+      jmp _loop
+
+    .user:
+      log input_buffer + 5
+      write nl
+      jmp _loop
+
+  set_color:
+    ; usage:
+    ; mov bl, your_color
+    ; mov cx, your_string_length
+    mov ah, 09h
+    int 10h
+    ret
+  
+  clear_screen:
+    mov ax, 3
+    int 10h
     ret
 
-get_string:
-  xor cl, cl
-    .loop:
-      mov ah, 0
-      int $16
+  print_string:
+    ; si = string pointer
+    ; bl = color
 
-      cmp al, $08
-      je .backspace
-   
-      cmp al, 0x0D
-      je .done
-   
-      cmp cl, 0x4D ; 77
-      je .loop
-   
+    lodsb
+    or al, al
+    jz .ret
+
+    cmp al, `\r`
+    je .writechar
+    cmp al, `\n`
+    je .writechar
+
+    ;pusha
+    mov cx, 1
+    call set_color
+    ;popa
+
+    jmp .writechar
+
+    jmp print_string
+
+    .writechar:
       mov ah, $0e
-      int 0x10
-
-      stosb
-      inc cl
-      jmp .loop
-
-    .backspace:
-      cmp cl, 0
-      je .loop
-
-      dec di
-      mov byte [di], 0
-      dec cl
-    
-      ; move the cursor back 1 position
-      mov ah, $0e
-      mov al, $08
       int $10
+      jmp print_string
 
-      ; remove the character at the current position
-      mov al, ' '
-      int $10
-
-      ; move the cursor back 1 position
-      mov al, $08
-      int $10
-
-      jmp .loop
-
-    .done:
-      mov al,0
-      stosb
-      
-      mov ah, $0e
-      mov al, 0x0D
-      int $10
-      mov al, 0x0A
-      int $10
-
+    .ret:
       ret
 
-cmp_string:
-  ; usage:
-  ; mov si, your_string_1
-  ; mov si, your_string_2
-  ; the carry flag is set to high if they match
+  get_string:
+    xor cl, cl
+      .loop:
+        mov ah, 0
+        int $16
+  
+        cmp al, $08
+        je .backspace
+   
+        cmp al, 0x0D
+        je .done
+   
+        cmp cl, 0x4D ; 77
+        je .loop
+   
+        mov ah, $0e
+        int 0x10
 
- .loop:
-  mov al, [si]
-  mov bl, [di]
-  cmp al, bl
-  jne .notequal
+        stosb
+        inc cl
+        jmp .loop
 
-  cmp al, cl ; the string ending
-  je .done
+      .backspace:
+        cmp cl, 0
+        je .loop
 
-  inc di
-  inc si
+        dec di
+        mov byte [di], 0
+        dec cl
+    
+        ; move the cursor back 1 position
+        mov ah, $0e
+        mov al, $08
+        int $10
 
-  jmp .loop
+        ; remove the character at the current position
+        mov al, ' '
+        int $10
 
-  .notequal:
-    clc
-    ret
+        ; move the cursor back 1 position
+        mov al, $08
+        int $10
 
-  .done:
-    stc
-    ret
+        jmp .loop
 
-len_string:
-  ; usage:
-  ; mov si, your_string
-  ; si is the string length now
+      .done:
+        mov al,0
+        stosb
+      
+        mov ah, $0e
+        mov al, 0x0D
+        int $10
+        mov al, 0x0A
+        int $10
 
-  mov di, 0
+        ret
 
-  .loop:
-    mov al, [si] ; get the contents of si into al
+  cmp_string:
+    ; usage:
+    ; mov si, your_string_1
+    ; mov di, your_string_2
+    ; the carry flag is set to high if they match
 
-    cmp al, 0x00 ; null character
-    je .done ; jump if true
+    .loop:
+      mov al, [si]
+      mov bl, [di]
+      cmp al, bl
+      jne .notequal
 
-    inc si ; increment the pointers
-    inc di
-    inc al
+      cmp al, cl ; the string ending
+      je .done
 
-    jmp .loop
+      inc di
+      inc si
 
-    .done:
-      mov si, di ; move the length to si
-      ret ; then retire
+      jmp .loop
+
+      .notequal:
+        clc
+        ret
+
+      .done:
+        stc
+        ret
+
+  len_string:
+    ; usage:
+    ; mov si, your_string
+    ; si is the string length now
+
+    mov di, 0
+
+    .loop:
+      mov al, [si] ; get the contents of si into al
+
+      cmp al, 0x00 ; null character
+      je .done ; jump if true
+
+      inc si ; increment the pointers
+      inc di
+
+      jmp .loop
+
+      .done:
+        mov si, di ; move the length to si
+        ret ; then retire
