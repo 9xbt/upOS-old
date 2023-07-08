7308051ba3f13d1c9eb901c4a3aeab57d3863404
MrDumbrava <109512837+9xbt@users.noreply.github.com>
2023-07-08 20:06:56 +0200

    more broken stuff

--- src/terminal.c
+++ src/terminal.c
@@ -8,28 +8,40 @@
 
 int CURSOR_X, CURSOR_Y;
 
-void print_string(char* text, int color){ 
+void print_string(char* text, int color) {
   int counter = 0;
-  int start = (CURSOR_Y * VGA_WIDTH + CURSOR_X) * 2;
 
   while (1) {
+    int start = (CURSOR_Y * VGA_WIDTH + CURSOR_X);
+
     if (*(text + counter) == '\0') {
       break;
     }
 
-    *((char*)0xB8000 + (counter * 2 + 1) + start) = color;
-    *((char*)0xB8000 + (counter * 2) + start) = *(text + counter);
+     if (*(text + counter) == '\n') {
+      CURSOR_X = 0;
+      CURSOR_Y ++;
+    }
+    
+    else {
+      print_char(*(text + start), color);
 
-    counter++;
-
-    CURSOR_X = counter;
-    if (CURSOR_X > VGA_WIDTH) {
+      CURSOR_X++;
+      if (CURSOR_X == VGA_WIDTH) {
         CURSOR_X = 0;
         CURSOR_Y++;
+      }
     }
+
+    counter++;
   }
 
   set_cursor_pos(CURSOR_X, CURSOR_Y);
+}
+
+void print_char(char c, int color) {
+  *((char*)0xB8000 + ((CURSOR_Y * VGA_WIDTH + CURSOR_X) * 2 + 1)) = color;
+  *((char*)0xB8000 + ((CURSOR_Y * VGA_WIDTH + CURSOR_X) * 2))     = c;
 }
 
 void set_cursor_pos(uint8_t x, uint8_t y) {
