cc4509b46db6e28e0f57c4716ad12ca7450e2c96
Winksplorer <winksplorer@gordae.com>
2023-07-14 18:48:14 -0700

    add gui into help

--- src/functions/help.asm
+++ src/functions/help.asm
@@ -2,7 +2,7 @@
 
 section .data
   msg_help_1: db `-- Functions --\r\n\0`
-  msg_help_2: db ` help - Shows all functions.\r\n about - Shows information about the project.\r\n clear - Clears the screen.\r\n echo - Echoes what you say.\r\n credits - Shows the credits.\r\n user - Sets your username for this session.\r\n\n\0`
+  msg_help_2: db ` help - Shows all functions.\r\n about - Shows information about the project.\r\n clear - Clears the screen.\r\n echo - Echoes what you say.\r\n credits - Shows the credits.\r\n user - Sets your username for this session.\r\n gui - Starts the experimental GUI interface\r\n\n\0`
 
   cmd_help: db `help\0`
   cmd_help_alias: db `?\0`
