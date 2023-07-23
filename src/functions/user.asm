1779ee55ca11a5e8c4d91a6f499178f25d74ab9f
Winksplorer <winksplorer@gordae.com>
2023-07-22 19:07:22 -0700

    Allow user and echo cmds to check if args are given

--- src/functions/user.asm
+++ src/functions/user.asm
@@ -2,10 +2,20 @@
 
 section .data
     cmd_user: db "user"
+    msg_user_no_args: db `No arguments specified. Required to change username.\r\n\0`
 
 section .test
     func_user:
+        mov si, input_buffer
+        call len_string
+        cmp si, 4
+        je .no_args_given
+
         mov si, input_buffer + 5
         mov di, user_buffer
         call str_copy
         ret
+
+        .no_args_given:
+            write msg_user_no_args
+            ret
