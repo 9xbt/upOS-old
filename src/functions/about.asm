cccd77f9c6b801ff6fe0c979d9d86e19cddc74b1
MrDumbrava <109512837+9xbt@users.noreply.github.com>
2023-08-17 23:30:10 +0200

    ekeleze is a cunt

--- src/functions/about.asm
+++ src/functions/about.asm
@@ -1,7 +1,7 @@
 %include "src/core.asm"
 
 section .data
-    msg_about_1: db `-- iDOS --\r\n\0`
+    msg_about_1: db `-- upOS --\r\n\0`
     msg_about_2: db ` Beta 1.2-dev\r\n Copyright (c) 2023 Mobren. All rights reserved.\r\n\n\0`
 
     cmd_about: db `about\0`
